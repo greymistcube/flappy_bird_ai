e63f3f42c4d08bb33e0b54a45a5a715b8717762a
Say Cheong <greymistcube@gmail.com>
2019-03-27 16:47:05 +0900

    more cleaning up code

--- flappy_bird_ai.py
+++ flappy_bird_ai.py
@@ -1,23 +1,41 @@
 import sys
+import random
 import pygame
 
-SIZE = WIDTH, HEIGHT = 640, 480
-GRAVITY = 0.5
-WHITE = 255, 255, 255
-
-ball = pygame.image.load("blue_ball.png")
-ball = pygame.transform.scale(ball, [x * 2 for x in ball.get_size()])
+import constants
 
 def reset_game():
-    global ballrect
+    global ball
+    global wall
     global velocity
     velocity = 0
-    ballrect = ball.get_rect()
-    ballrect = ballrect.move([160, 160])
+    ball = Ball()
+    wall = Wall()
+
+def load_image(file):
+    image = pygame.image.load(file)
+    image = pygame.transform.scale(image, [x * 2 for x in image.get_size()])
+    return image
+
+class Ball:
+    image = load_image("blue_ball.png")
+
+    def __init__(self):
+        self.rect = self.image.get_rect()
+        self.rect = self.rect.move(constants.START_POSITION)
+        return
+
+class Wall:
+    image = load_image("brick_wall.png")
+
+    def __init__(self):
+        self.rect = self.image.get_rect()
+        self.rect = self.rect.move([480,240])
+        return
 
 if __name__ == "__main__":
     pygame.init()
-    screen = pygame.display.set_mode(SIZE)
+    screen = pygame.display.set_mode(constants.SIZE)
     clock = pygame.time.Clock()
 
     # initialize game before starting
@@ -33,19 +51,20 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 sys.exit()
         
-        ballrect = ballrect.move((0, velocity))
-        velocity = velocity + GRAVITY
+        ball.rect = ball.rect.move((0, velocity))
+        velocity = velocity + constants.GRAVITY
 
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[pygame.K_SPACE]:
-            velocity = -10
+            velocity = constants.JUMP_VELOCITY
 
         # check if the pc is out of bounds
-        if ballrect.top < 0 or ballrect.bottom > HEIGHT:
+        if ball.rect.top < 0 or ball.rect.bottom > constants.HEIGHT:
             reset_game()
         
         # draw screen
-        screen.fill(WHITE)
-        screen.blit(ball, ballrect)
+        screen.fill(constants.WHITE)
+        screen.blit(ball.image, ball.rect)
+        screen.blit(wall.image, wall.rect)
         pygame.display.flip()
 
